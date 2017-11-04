7a69a3a0df2f2ed866fed5d897decd3d80b43a3f
wiserubi <wiserubi@gmail.com>
2017-11-04 17:46:01 +0900

    매물등록추가

--- app/src/main/java/com/shinhan/kdh1/myrealestate/RegisterActivity.java
+++ app/src/main/java/com/shinhan/kdh1/myrealestate/RegisterActivity.java
@@ -1,16 +1,32 @@
 package com.shinhan.kdh1.myrealestate;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
 
 public class RegisterActivity extends AppCompatActivity {
     private final String[] slocation1 = {"서울마포구", "서울강남구", "서울서초구", "서울송파", "서울성북"};
-    private final String[] slocation2= {"염리3구역", "공덕1구역", "아현1구역", "잠실5단지", "북아현3구역"};
+    private final String[] slocation2 = {"염리3구역", "공덕1구역", "아현1구역", "잠실5단지", "북아현3구역"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,11 +36,11 @@ public class RegisterActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, slocation2);
 
         Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
 
         spinner1.setPrompt(" 시-구선택");
         spinner1.setAdapter(adapter1);
 
-        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
         spinner2.setPrompt(" 지역선택");
         spinner2.setAdapter(adapter2);
 
@@ -63,5 +79,122 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+    }
+    // option1 체크박스가 눌렸을 때
+/*
+    public String Checked(View v) { // 체크되었을 때 동작할 메소드 구현
+        // TODO Auto-generated method stub
+        CheckBox option1 = (CheckBox) findViewById(R.id.public); // option1체크박스
+        // 선언
+        CheckBox option2 = (CheckBox) findViewById(R.id.jisang); // option1체크박스
+        // 선언
+
+        String resultText = ""; // 체크되었을 때 값을 저장할 스트링 값
+        if (option1.isChecked()) { // option1 이 체크되었다면
+            resultText = "1";
+        }
+        if (option2.isChecked()) {
+            resultText = "1"; // option2 이 체크되었다면
+        }
+
+        return resultText; // 체크된 값 리턴
+    }
+*/
+    public void RegisterOffer(View view) {
+        Spinner locationSpinner = (Spinner) findViewById(R.id.spinner1);
+        Spinner locationSpinner2 = (Spinner) findViewById(R.id.spinner2);
+        EditText totalamtText = (EditText) findViewById(R.id.tot_amt);
+        EditText premiumText = (EditText) findViewById(R.id.premium);
+        EditText rentText = (EditText) findViewById(R.id.rent);
+        EditText loanText = (EditText) findViewById(R.id.loan);
+        EditText movingText = (EditText) findViewById(R.id.moving);
+        EditText originalamtText = (EditText) findViewById(R.id.original_amt);
+
+        new registerOffer().execute(
+                "http://172.16.2.14:52273/user/offer",
+                locationSpinner.getSelectedItem().toString(),
+                locationSpinner2.getSelectedItem().toString(),
+                totalamtText.getText().toString(),
+                premiumText.getText().toString(),
+                rentText.getText().toString(),
+                loanText.getText().toString(),
+                movingText.getText().toString(),
+                originalamtText.getText().toString());
+    }
+
+    class registerOffer extends AsyncTask<String, String, String> {
+        ProgressDialog dialog = new ProgressDialog(RegisterActivity.this);
+
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder output = new StringBuilder();
+            try {
+                URL url = new URL(params[0]);
+                JSONObject postDataParams = new JSONObject();
+                //postDataParams.put("user_id", params[1]);
+                postDataParams.put("location_si", params[1]);
+                postDataParams.put("location_name", params[2]);
+                postDataParams.put("total_amt", params[3]);
+                postDataParams.put("original_amt", params[4]);
+                postDataParams.put("premium", params[5]);
+                postDataParams.put("rent", params[6]);
+                postDataParams.put("loan", params[7]);
+                postDataParams.put("migration_fee", params[8]);
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                if (conn != null) {
+                    conn.setConnectTimeout(10000);
+                    conn.setRequestMethod("POST");
+                    conn.setDoInput(true);
+                    conn.setDoOutput(true);
+                    OutputStream os = conn.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(
+                            new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(getPostDataString(postDataParams));
+                    writer.flush();
+                    writer.close();
+                    os.close();
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+                    String line = null;
+                    while (true) {
+                        line = reader.readLine();
+                        if (line == null) break;
+                        output.append(line);
+                    }
+                    reader.close();
+                    conn.disconnect();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return output.toString();
+        }
+
+
+        public String getPostDataString(JSONObject params) throws Exception {
+
+            StringBuilder result = new StringBuilder();
+            boolean first = true;
+
+            Iterator<String> itr = params.keys();
+
+            while (itr.hasNext()) {
+
+                String key = itr.next();
+                Object value = params.get(key);
+
+                if (first)
+                    first = false;
+                else
+                    result.append("&");
+
+                result.append(URLEncoder.encode(key, "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+            }
+            return result.toString();
+        }
     }
 }
--- app/src/main/java/com/shinhan/kdh1/myrealestate/ViewActivity.java
+++ app/src/main/java/com/shinhan/kdh1/myrealestate/ViewActivity.java
@@ -1,17 +1,74 @@
 package com.shinhan.kdh1.myrealestate;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewActivity extends AppCompatActivity {
+    ListView listView = null;
+    class Item {
+        int image;
+        String title;
+        String text;
+        Item(int image, String title, String text) {
+            this.image = image;
+            this.title = title;
+            this.text = text;
+        }
+    }
+    ArrayList<Item> itemList = new ArrayList<Item>();
+    class ItemAdapter extends ArrayAdapter {
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater layoutInflater =
+                        (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = layoutInflater.inflate(R.layout.activity_view, null);
+            }
+            TextView text1View = (TextView)convertView.findViewById(R.id.title);
+            TextView text2View = (TextView)convertView.findViewById(R.id.text);
+            Item item = itemList.get(position);
 
+            text1View.setText(item.title);
+            text2View.setText(item.text);
+            return convertView;
+        }
+
+        public ItemAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List objects) {
+            super(context, resource, objects);
+        }
+    }
+    ItemAdapter itemAdpater = null;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+        new LoadUserList().execute("http://172.16.2.14:52273/offer");
 
         Button Button1 = (Button) findViewById(R.id.ButtonHome); //해당 버튼을 지정합니다.
         Button1.setOnClickListener(new View.OnClickListener() {
@@ -49,5 +106,65 @@ public class ViewActivity extends AppCompatActivity {
             }
         });
 
+        itemAdpater = new ItemAdapter(ViewActivity.this, R.layout.activity_view,itemList);
+        listView.setAdapter(itemAdpater);
+
+    }
+    class LoadUserList extends AsyncTask<String,String,String> {
+        ProgressDialog dialog = new ProgressDialog(ViewActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            dialog.setMessage("사용자 목록 로딩 중...");
+            dialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {//s-->서버에서 받은 JSON문자열
+            dialog.dismiss();
+            try{
+                JSONArray array = new JSONArray(s);
+                ArrayList<String> strings = new ArrayList<String>();
+                for(int i =0 ; i < array.length(); i++){
+                    JSONObject obj = array.getJSONObject(i);
+                    strings.add(obj.getString("location_si"));
+                    strings.add(obj.getString("location_name"));
+                    strings.add(obj.getString("total_amt"));
+                }
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                        ViewActivity.this, android.R.layout.simple_list_item_1,strings);
+                ListView listView = (ListView)findViewById(R.id.listview);
+                listView.setAdapter(adapter);
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder output = new StringBuilder();
+            try{
+                URL url = new URL(params[0]);
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                if(conn!=null){
+                    conn.setConnectTimeout(10000);
+                    conn.setRequestMethod("GET");
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+                    String line = null;
+                    while(true) {
+                        line = reader.readLine();
+                        if (line == null) break;
+                        output.append(line);
+                    }
+                    reader.close();
+                    conn.disconnect();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return output.toString();
+        }
     }
 }
